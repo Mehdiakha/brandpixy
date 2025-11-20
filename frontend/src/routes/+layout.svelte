8c9288bd095f032c18e35a40e738f59c87ad26ba
Mehdiakha <akhabbarmehdi@gmail.com>
2025-11-20 21:38:53 +0100

    Move favicon to static folder to fix build error

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script>
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.svg" />
 </svelte:head>
 
 {@render children()}
