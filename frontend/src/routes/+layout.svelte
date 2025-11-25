5ea12b0bb9c2e2a35ed772486095c8031daa0a82
Mehdiakha <akhabbarmehdi@gmail.com>
2025-11-25 18:42:50 +0100

    tracker script

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
 	import '../app.css';
+	import { dev } from '$app/environment';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
 	let { children } = $props();
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <svelte:head>
