2e6c141f960d0c88cda92923cc84a7e873c8db3d
Mehdiakha <akhabbarmehdi@gmail.com>
2025-11-25 21:37:25 +0100

    ui fix

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import sys
+import os
+
+# Add the parent directory to sys.path so we can import 'backend'
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 from backend.app.main import app
